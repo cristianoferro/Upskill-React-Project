d40332c56f51c9fed68b9b2b774fbccc11402b96
Cristiano Ferro <cristianobferro@gmail.com>
2021-02-23 15:29:33 +0000

    animated pages presence

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import Login from './components/Login';
-import Initial from './components/Initial'
+import Initial from './components/Initial';
+import { AnimatePresence } from 'framer-motion';
+import "./styles/App.scss";
 
 function App() {
+  
+  // give the location of the router
+  const location = useLocation();
+
   return (
-    <>
-        <Switch>
-          <Route path="/">
-            <Login/>
-          </Route>
-          <Route path="/initial">
-            <Initial/>
-          </Route>
-        </Switch>
+    <>  
+        {/* Animates when page disappears */}
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.key}>
+            <Route path="/"exact>
+              <Login/>
+            </Route>
+            <Route path="/initial">
+              <Initial/>
+            </Route>
+          </Switch>
+        </AnimatePresence>
+        
     </>
   );
 }
