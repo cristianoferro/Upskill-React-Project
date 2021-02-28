f0964a8d94e5f2759385c0571ca70eff9966fb2e
Cristiano Ferro <cristianobferro@gmail.com>
2021-02-28 20:00:01 +0000

    corrected logo viewbox measures

--- src/components/LogoAnimation.js
+++ src/components/LogoAnimation.js
@@ -25,7 +25,7 @@ const Login = () => {
 
     return(
         
-          <motion.svg className="svg-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080"
+          <motion.svg className="svg-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 843 335"
             variants={pathVariants}
             initial="hidden"
             animate="visible"
