8439aebe31e2db7a66ccb87f59c1e7b768bf34a5
Cristiano Ferro <cristianobferro@gmail.com>
2021-03-15 08:34:40 +0000

    deleted duplicated MyKitchen

--- src/components/MyKitchen.js
+++ src/components/MyKitchen.js
@@ -1,14 +1,19 @@
 import {Link} from 'react-router-dom';
 
-const MyKitchen = () => {
+const MyKitchen = ({inputRef, submitClickHandler, submit}) => {
     return(
+        
         <div>
-        <nav className='my-kitchen-nav' >
-            <Link to="/inventário" className='nav-item' >Inventário</Link>
-            <Link to="/menus" className='nav-item' >Menus</Link>
-            <Link to="/receitas" className='nav-item' >Receitas</Link>
-            <Link to="/alertas" className='nav-item' >Alertas</Link>
-            <Link to="" className='nav-item' >Search</Link>
+        <form>
+            <input type="text" placeholder="Item" ref={inputRef}/>
+            <button onClick={submitClickHandler} type="submit">{submit}</button>
+        </form>
+        <nav className="mykitchen-menu">
+            <Link to="/inventario" >Inventários</Link>
+            <Link to="/menus" >Menus</Link>
+            <Link to="/receitas" >Receitas</Link>
+            <Link to="/alertas" >Alertas</Link>
+            <Link to="">Search</Link>
         </nav>
         </div> 
     )
