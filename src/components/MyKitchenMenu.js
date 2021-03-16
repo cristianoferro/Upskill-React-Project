7837b375f6f3e6e59ff17967edc8badb59ec8dc1
Cristiano Ferro <cristianobferro@gmail.com>
2021-03-16 23:20:47 +0000

    adapting styles + to media queries

--- src/components/MyKitchenMenu.js
+++ src/components/MyKitchenMenu.js
@@ -4,10 +4,7 @@ const MyKitchenMenu = () => {
     return(
             <nav className="mykitchen-menu box-element">
                 <Link to="/inventario" >Inventários</Link>
-                <Link to="/menus" >Menus</Link>
                 <Link to="/receitas" >Receitas</Link>
-                <Link to="/alertas" >Alertas</Link>
-                <Link to="">Search</Link>
             </nav>
     )
 }
