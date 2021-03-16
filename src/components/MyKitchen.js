47b0246c5ee7324383f6132342ca7ebff6d03ddd
Cristiano Ferro <cristianobferro@gmail.com>
2021-03-16 19:43:33 +0000

    list of items and view details functionality

--- src/components/MyKitchen.js
+++ src/components/MyKitchen.js
@@ -2,8 +2,10 @@ import MyKitchenMenu from './MyKitchenMenu';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from "react";
 
-import UpperBar from './UpperBar'
-import SearchBar from './SearchBar'
+import UpperBar from './UpperBar';
+import SearchBar from './SearchBar';
+import defaultItemImg from '../media/images/0.jpg';
+import Close from './Close';
 
 const useStorageRetriever = (key, initialState) => {
     const [value, setValue] = useState(
@@ -37,6 +39,114 @@ const MyKitchen = ({inputRef, submitClickHandler, submit, globalVariants, childr
             Custo_Unitário:165.66,
             Moeda_do_Preço:"Euro",
             Custo_Total:165.66,
+        },
+        {
+            itemID: 2,
+            Nome_do_Item:"FORNO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:4,
+            Custo_Unitário:1999.28,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:"7997,12",
+        },
+        {   
+            itemID: 3,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
+        },
+        {
+            itemID: 4,
+            Nome_do_Item:"FORNO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:4,
+            Custo_Unitário:1999.28,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:"7997,12",
+        },
+        {   
+            itemID: 5,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
+        },
+        {
+            itemID: 6,
+            Nome_do_Item:"FORNO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:4,
+            Custo_Unitário:1999.28,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:"7997,12",
+        },
+        {   
+            itemID: 7,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
+        },
+        {   
+            itemID: 8,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
+        },
+        {
+            itemID: 9,
+            Nome_do_Item:"FORNO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:4,
+            Custo_Unitário:1999.28,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:"7997,12",
+        },
+        {   
+            itemID: 10,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
+        },
+        {
+            itemID: 11,
+            Nome_do_Item:"FORNO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:4,
+            Custo_Unitário:1999.28,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:"7997,12",
+        },
+        {   
+            itemID: 12,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
+        },
+        {   
+            itemID: 13,
+            Nome_do_Item:"MÁQUINA DE GELO",
+            Tipo_de_Item:"EQUIPAMENTO DE CAPITAL",
+            Quantidade_Necessária:1,
+            Custo_Unitário:165.66,
+            Moeda_do_Preço:"Euro",
+            Custo_Total:165.66,
         }
         ];
         
@@ -57,6 +167,9 @@ const MyKitchen = ({inputRef, submitClickHandler, submit, globalVariants, childr
         item.Nome_do_Item.toLowerCase().includes(searchTerm.toLowerCase()) ||
         item.Tipo_de_Item.toLowerCase().includes(searchTerm.toLowerCase()) 
     );
+
+    const [expanded, setExpanded] = useState();
+
     return(
         
         <motion.div
@@ -75,28 +188,53 @@ const MyKitchen = ({inputRef, submitClickHandler, submit, globalVariants, childr
                     <input type="text" placeholder="Item" ref={inputRef}/>
                     <button onClick={submitClickHandler} type="submit">{submit}</button>
                 </form> */}
-                <List list={searchedInventario} />
+
+                <h2>Resultados Inventário</h2>
+                <List list={searchedInventario} expanded={expanded} setExpanded={setExpanded}/>
                 <MyKitchenMenu/>
                 
         </motion.div> 
     )
 }
 
-const List = ({ list }) =>
-    list.map(item => <Item key={item.itemID} item={item} />);
+const List = ({ list, expanded, setExpanded}) => (
+        <div className="list-results">
+            {list.map((item, id) => <Item key={item.itemID} id={id} item={item} expanded={expanded} setExpanded={setExpanded}  />)}
+        </div>
+        
+    )
 
-const Item = ({ item }) =>(
-    <div>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      
-      <span>{item.Nome_do_Item}</span>
-      <span>{item.Tipo_de_Item}</span>
-      <span>{item.Quantidade_Necessária}</span>
-      <span>{item.Custo_Unitário}</span>
-      <span>{item.Moeda_do_Preço}</span>
-      <span>{item.Custo_Total}</span>
-    </div>
+const Item = ({ k, id, item, expanded, setExpanded}) => {
+    const [itemIsClosed, setItemIsClosed] = useState(true);
+    const closeItem = () =>{
+        setItemIsClosed(true)
+    }
+    const clickItem = (id) => {
+        setExpanded(id)
+        setItemIsClosed(false)
+    }
+    return(
+        <>
+            <div className={(id + 1) % 4 === 0 ? "item-result no-margin-right" : "item-result"} 
+                onClick={() => clickItem(id)}>
+                <div>
+                    <span>{item.Nome_do_Item}</span>
+                    <span>{item.Nome_do_Item}</span>
+                </div>
+
+            <div className="item-img"><img src={item.image ? item.image : defaultItemImg} alt=""/></div>
+            </div>
+            <div className={id === expanded && !itemIsClosed ? "item-detail" : "item-detail-hidden"}>
+                <Close closeItem={closeItem} />
+                <span>{item.Nome_do_Item}</span>
+                <span>{item.Tipo_de_Item}</span>
+                <span>{item.Quantidade_Necessária}</span>
+                <span>{item.Custo_Unitário}</span>
+                <span>{item.Moeda_do_Preço}</span>
+                <span>{item.Custo_Total}</span>
+                <div className="item-img-detail"><img src={item.image ? item.image : defaultItemImg} alt=""/></div>
+            </div>
+        </>
   );
+}
 export default MyKitchen;
