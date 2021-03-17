f20ace9737040505685808cb81c3cac1556c64da
Cristiano Ferro <cristianobferro@gmail.com>
2021-03-17 04:20:07 +0000

    filters tests

--- src/components/MyKitchen.js
+++ src/components/MyKitchen.js
@@ -9,6 +9,7 @@ import defaultItemImg from '../media/images/0.jpg';
 import Close from './Close';
 import inventory from '../data/inventory.json';
 import alerts from '../data/alerts.json';
+import recipes from '../data/Recipes.json';
 
 const useStorageRetriever = (key, initialState) => {
     const [value, setValue] = useState(
@@ -60,32 +61,126 @@ const MyKitchen = ({inputRef, submitClickHandler, submit, globalVariants, childr
     };
 
     // filtered items
-    const searchedInventario = inventory.inventory[0].inventoryitems.filter(inventoryItem =>
-        inventoryItem.itemsinventory[0].labels.displayName.includes(searchTerm.toLowerCase()) ||
-        inventoryItem.itemsinventory[0].labels.displayName.includes(searchTerm.toLowerCase()) 
+    // const searchedInventario = alerts.alerts[0].alertsitems.filter(Alerts =>
+    //     // Alerts.itemsalert[0].labels.displayName.includes("Lacticínios")
+    //     // console.log("LOG",Alerts.itemsalert[0].labels.displayName.includes("Lacticínios"))
+    
+    //     //filtrar por nome do alerta OU
+    //     Alerts.itemsalert[0].labels.displayName.includes(searchTerm.toLowerCase()) ||
+    //     //filtrar por números (converter sempre para string)
+    //     Alerts.itemsalert[0].quantitySize.quantity.toString().includes(searchTerm.toLowerCase()) 
+    // );
+
+    // FILTROS ALERTAS
+    //////
+    //////
+    //////
+    //////
+    //////
+    //////
+    //////
+    //////
+    
+    // Teste para aceder aos campos dos alertas
+    // alerts.alerts[0].alertsitems.map((searchedAlerts, key) => {
+    //   console.log(searchedAlerts.itemsalert[0].labels.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].labelsitems.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].quantitySize.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].quantitySize.quantity);
+    //   console.log(searchedAlerts.itemsalert[0].quantityStock.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].quantityStock.quantity);
+    //   console.log(searchedAlerts.itemsalert[0].manteinanceAlert.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].manteinanceAlert.quantity);
+    //   console.log(searchedAlerts.itemsalert[0].buyAlert.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].buyAlert.quantity);
+    //   console.log(searchedAlerts.itemsalert[0].urgentBuy.displayName);
+    //   console.log(searchedAlerts.itemsalert[0].urgentBuy.description);
+    //   console.log(searchedAlerts.itemsalert[0].urgentBuy.alertSignal);
+    //   console.log(searchedAlerts.itemsalert[0].mediaFormat);
+    //   console.log(searchedAlerts.itemsalert[0].locationAssociation.category);
+    //   console.log(searchedAlerts.itemsalert[0].sourceUrl);
+    //   console.log(key)
+    //   console.log("---------------------------------------------")
+    // })
+
+    // Teste de filtro
+    // const newFilter = alerts.alerts[0].alertsitems.filter(Alerts =>
+    //     Alerts.itemsalert[0].labels.displayName.includes(searchTerm.toLowerCase()) ||
+    //     Alerts.itemsalert[0].labelsitems.displayName.includes(searchTerm.toLowerCase()) 
+    // );
+
+    const searchedInventario = alerts.alerts[0].alertsitems.filter(Alerts =>
+        Alerts.itemsalert[0].labels.displayName.includes(searchTerm.toLowerCase()) ||
+        Alerts.itemsalert[0].quantitySize.quantity.toString().includes(searchTerm.toLowerCase()) 
     );
-    // console.log(searchedInventario);
-    alerts.alerts[0].alertsitems.map((searchedAlerts, key) => {
-      console.log(searchedAlerts.itemsalert[0].labels.displayName);
-      console.log(searchedAlerts.itemsalert[0].labelsitems.displayName);
-      console.log(searchedAlerts.itemsalert[0].quantitySize.displayName);
-      console.log(searchedAlerts.itemsalert[0].quantitySize.quantity);
-      console.log(searchedAlerts.itemsalert[0].quantityStock.displayName);
-      console.log(searchedAlerts.itemsalert[0].quantityStock.quantity);
-      console.log(searchedAlerts.itemsalert[0].manteinanceAlert.displayName);
-      console.log(searchedAlerts.itemsalert[0].manteinanceAlert.quantity);
-      console.log(searchedAlerts.itemsalert[0].buyAlert.displayName);
-      console.log(searchedAlerts.itemsalert[0].buyAlert.quantity);
-      console.log(searchedAlerts.itemsalert[0].urgentBuy.displayName);
-      console.log(searchedAlerts.itemsalert[0].urgentBuy.description);
-      console.log(searchedAlerts.itemsalert[0].urgentBuy.alertSignal);
-      console.log(searchedAlerts.itemsalert[0].mediaFormat);
-      console.log(searchedAlerts.itemsalert[0].locationAssociation.category);
-      console.log(searchedAlerts.itemsalert[0].sourceUrl);
-      console.log(key)
-      console.log("---------------------------------------------")
+
+    // const vegetarianFilter = recipes.recipes[0].recipessectionsFilter.filter(searchedSections => {
+    //       searchedSections.itemsrecipessectionsFilter.filter(searchedRecipes => {
+    //         searchedRecipes.labelsrecipe.displayName.includes(searchTerm.toLowerCase())
+    //       })
+    // })
+
+    // const searchedInventario = recipes.recipes[0].recipessectionsFilter.filter(searchedSections => {
+    //     searchedSections.itemsrecipessectionsFilter.filter(searchedRecipes => {
+    //       searchedRecipes.labelsrecipe.displayName.includes(searchTerm.toLowerCase())
+    // });
+
+    const abc = recipes.recipes[0].recipessectionsFilter.filter(searchedSections => {
+        searchedSections.itemsrecipessectionsFilter.filter(searchedRecipes => {
+            searchedRecipes.nutritionFactsrecipe.calories.upperAmount.toString().includes(searchTerm.toLowerCase()) 
+        })
     })
-    console.log("alertas!", alerts.alerts[0].alertsitems[0].itemsalert[0]);
+
+    console.log(abc);
+        // recipes.recipes[0].recipessectionsFilter.map((searchedSections, key) => {
+        //     console.log(key, searchedSections.itemsrecipessectionsFilter[0].labelsrecipe.displayName)
+        // })
+    // Teste para aceder aos campos das receitas
+    // recipes.recipes[0].recipessectionsFilter.map((searchedSections, key) => {
+    //   searchedSections.itemsrecipessectionsFilter.map((searchedRecipes, key2) =>{
+    //     console.log("abc",searchedRecipes.labelsrecipessectionsFilter);
+    //     console.log(searchedRecipes.labelsrecipe.displayName);
+        // console.log(searchedRecipes.labelsrecipe.description);
+        // console.log(searchedRecipes.nutritionFactsrecipe.calories.lowerAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.calories.upperAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.calories.unit);
+        // console.log(searchedRecipes.nutritionFactsrecipe.totalFat.lowerAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.totalFat.upperAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.totalFat.unit);
+
+        // console.log(searchedRecipes.nutritionFactsrecipe.cholesterol.lowerAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.cholesterol.upperAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.cholesterol.unit);
+        // console.log(searchedRecipes.nutritionFactsrecipe.sodium.lowerAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.sodium.upperAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.sodium.unit);
+        // console.log(searchedRecipes.nutritionFactsrecipe.totalCarbohydrate.lowerAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.totalCarbohydrate.upperAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.totalCarbohydrate.unit);
+        // console.log(searchedRecipes.nutritionFactsrecipe.protein.lowerAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.protein.upperAmount);
+        // console.log(searchedRecipes.nutritionFactsrecipe.protein.unit);
+        // searchedRecipes.ingredientsrecipe[0].labels.map((ingredients) =>{
+        //     console.log(ingredients.displayName)
+            // console.log(ingredients.description)
+        // })
+    //     console.log("key2",key2)
+    //     console.log("********")
+    //     console.log(searchedRecipes.servesNumPeoplerecipe);
+    //     console.log(searchedRecipes.preparationMethodsrecipe[0]);
+    //     console.log(searchedRecipes.portionSizerecipe.quantity);
+    //     console.log(searchedRecipes.portionSizerecipe.unit.displayName);
+    //     console.log(searchedRecipes.mediaFormatrecipe);
+    //     console.log(searchedRecipes.locationAssociationrecipe.category);
+    //     console.log(searchedRecipes.sourceUrlrecipe);
+    //   })
+
+    //   console.log("key",key)
+    //   console.log("---------------------------------------------")
+    // })
+
+    // console.log(newFilter);
+    
 
     const [expanded, setExpanded] = useState();
     
